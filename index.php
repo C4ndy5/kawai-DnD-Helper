ce9f9ae14cbf9f49e12d8786b4b99b56d4dc17f6
C4ndy5 <113103714+C4ndy5@users.noreply.github.com>
2022-10-03 23:09:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous">
+        </script>
+
+        <title>!!!AWESOME TRG!!!</title>
+    </head>
+    <body>
+        <div class="main-menu">
+            <div class="fir-menu-form">
+                <form>
+                    <input type="text" name="damage" placeholder="Профиль урона: ">
+                    <input type="text" name="rec" placeholder="Точность: ">
+                    <input type="text" name="shot-count" placeholder="Кол-во выстрелов: ">
+                </form>
+            </div>
+
+            <div class="sec-menu-result">
+                
+            </div>
+        </div>
+    </body>
+<html>
